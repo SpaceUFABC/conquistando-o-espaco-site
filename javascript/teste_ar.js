709d414ff5f7f279b85747175258a3e1b576438c
Marcelo Quesada <m.mergulhao@hotmail.com>
2025-12-04 19:57:36 -0300

    Added AR test

--- javascript/teste_ar.js
+++ javascript/teste_ar.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
+import {ARButton} from 'three/examples/jsm/Addons.js';
 import '../style.css'
-import { ARButton } from 'https://github.com/mrdoob/three.js/blob/dev/examples/jsm/webxr/ARButton.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 
 const sizes = {
     width: window.innerWidth,
@@ -10,23 +12,32 @@ const sizes = {
 // cena
 const scene = new THREE.Scene()
 
-// objeto (geometria e material)
-const cubeGeometry = new THREE.BoxGeometry(1,1,1)
-const cubeMaterial = new THREE.MeshBasicMaterial({color: "red"})
-// Malha final
-const cubeMesh = new THREE.Mesh(
-    cubeGeometry,
-    cubeMaterial
-)
-scene.add(cubeMesh)
+
+// importar geometria GLB
+const loader = new GLTFLoader();
+loader.load("../modelos/juno.glb", function(glb){
+    console.log(glb);
+    const root = glb.scene;
+    root.position.set(3, -2, -10)
+    scene.add(root);
+}, function(xhr){}, function(error){console.log("Error loading the model")})
 
 // luz
-const light = new THREE.PointLight(0xffffff, 1, 100)
-scene.add(light)
+// const light = new THREE.PointLight(0xffffff, 1000, 100000)
+// light.position.set(0, 10, 0)
+// scene.add(light)
+const light1 = new THREE.DirectionalLight(0xffffff, 50)
+light1.position.set(0, 10, 0)
+scene.add(light1)
+const light2 = new THREE.DirectionalLight(0xffffff, 1)
+light2.position.set(3, -10, 0)
+scene.add(light2)
+const light3 = new THREE.AmbientLight(0xffffff, 50)
+scene.add(light3)
 
 // camera
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 1, 30)
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.5, 100)
 camera.position.z = 5 // mover para poder enxergar o objeto
 scene.add(camera)
 
@@ -34,12 +45,22 @@ scene.add(camera)
 const canvas = document.querySelector(".webgl")
 const renderer = new THREE.WebGLRenderer({canvas})
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+renderer.xr.enabled = true;
+document.body.appendChild(ARButton.createButton(renderer))
 
-// AR
-const button = ARButton.createButton(renderer);
-document.body.appendChild(button);
-console.log(button)
+function animate(){
+    // requestAnimationFrame(animate);
+    // renderer.render(scene, camera);
+    renderer.setAnimationLoop(render);
+}
+animate()
+
+function render(){
+    renderer.render(scene, camera);
+}
+
+// Controle
+const controls = new OrbitControls(camera, canvas);
 
 // resize
 window.addEventListener("resize", () => {
@@ -48,8 +69,8 @@ window.addEventListener("resize", () => {
     sizes.height = window.innerHeight;
 
     //Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.render(scene, camera)
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.render(scene, camera);
 })
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+// vite.config.mjs
+import { defineConfig } from 'vite'
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default defineConfig({
+  server: {
+    host: true,
+    port: 5173,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'server.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'server.crt'))
+    }
+  }
+})
